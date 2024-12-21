7b927af8f6c7345dd9d424fbd101500e8e473cf7
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2024-12-21 15:45:15 +0530

    hello world

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
       int first = 10;
       int second = 20;
   
-      // add two numbers  
+      // add two numbers  g
       int sum = first + second;
       System.out.println(first + " + " + second + " = "  + sum);
     }
