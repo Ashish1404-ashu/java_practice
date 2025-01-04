c64899aa665643c97e1a9e18c2eb8a6672076e16
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-04 08:17:59 +0530

    tloops

--- Tloops.java
+++ Tloops.java
@@ -1,9 +1,14 @@
 public class Tloops {
     public static void main(String[] args) {
-        for(int i=0;i<=5;i++){
-        for(int j=0;j<=i;j++)
-        System.out.println("1");
-        System.out.println(i +" ");
-     }
+        int n = 5;  
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
