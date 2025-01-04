440209ea67405f15c4085550e6092727573c248a
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-05 01:52:40 +0530

    add

--- Adds.java
+++ Adds.java
@@ -1,7 +1,6 @@
 public class Adds {
    public static void main(String[] args) {
     int num1 =5 , num2= 10;
-    int sum = num1 +num2;
     System.out.println(num1+num2);
    } 
 }
--- add.java
+++ add.java
@@ -2,7 +2,6 @@ public class add {
     public static void main(String[]args){
         int num1 =5 , num2= 10;
        int sum=num1+num2;
-        Object system;
-                System.out.println(sum);
+        System.out.println(sum);
     }
 }
