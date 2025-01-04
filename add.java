466e8c224fda9c097e92eee45e89e7f2a43ebbfd
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-05 01:43:11 +0530

    sum

--- add.java
+++ add.java
@@ -1,7 +1,8 @@
 public class add {
     public static void main(String[]args){
         int num1 =5 , num2= 10;
-        sum=num1+num2;
-        system.out.println("sum of "+num1+"and"+num2 +"is"+sum)
+       int sum=num1+num2;
+        Object system;
+                System.out.println(sum);
     }
 }
