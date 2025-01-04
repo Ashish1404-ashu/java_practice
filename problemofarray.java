2e33f856d8613dd4586f08870b1a88876c14f336
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-04 08:36:16 +0530

    maximum

--- problemofarray.java
+++ problemofarray.java
@@ -2,13 +2,13 @@ public class problemofarray {
    public static void main(String[] args) {
     int numbers[] ={23,12,6,7,15,3,5,56};
 
-    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
 
     for(int number:numbers){
-        if(number <min){
-            min = number;
+                if(number >max){
+            max = number;
         }
     }
-    System.out.println("minimum is"+min);
+    System.out.println("maximum is"+ max);
    } 
 }
