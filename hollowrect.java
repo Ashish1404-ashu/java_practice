b71bcf8fd1b766c9c3de3ab127821cdbf5e41e9c
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-30 01:42:29 +0530

    hollowrectangle

---
+++ hollowrect.java
@@ -0,0 +1,15 @@
+public class hollowrect {
+    public static void main(String[] args) {
+        for (int i=1; i<=4; i++){
+            for (int j=1; j<=5 ; j++){
+                if(i == 1 j==1 i==n j==m){
+                    System.out.print("*");
+                } else{
+                        System.out.println(" ");
+ 
+                    }
+    }
+}
+System.out.println();
+    }
+}
