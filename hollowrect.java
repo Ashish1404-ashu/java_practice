6dba4c4eccf4238551b3e962145a9f2541964d69
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-01-30 01:50:42 +0530

    halfpyramid

--- hollowrect.java
+++ hollowrect.java
@@ -2,9 +2,11 @@ public class hollowrect {
     public static void main(String[] args) {
         for (int i=1; i<=4; i++){
             for (int j=1; j<=5 ; j++){
-                if(i == 1 j==1 i==n j==m){
-                    System.out.print("*");
-                } else{
+               if (condition:var(boolean)) {
+                
+               } else {
+                
+               }
                         System.out.println(" ");
  
                     }
