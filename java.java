dfca699a89f24f1ce47253de66233f02d80ee3e1
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-02-16 13:12:55 +0530

    java

---
+++ java.java
@@ -0,0 +1,14 @@
+public class java {
+    public static void main(String[] args) {
+        
+        int fisrt = 15;
+        int second= 25;
+
+        int sum = fisrt +second ;
+
+        System.out.println(sum);
+
+
+
+    }
+}
