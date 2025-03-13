e9f7c0eb14cf5961b79dbebbd1098b03f00826bb
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-03-13 22:22:11 +0530

    janu

---
+++ java practice/Adds.java
@@ -0,0 +1,6 @@
+public class Adds {
+   public static void main(String[] args) {
+    int num1 =5 , num2= 10;
+    System.out.println(num1+num2);
+   } 
+}
---
+++ java practice/LearnLoops.java
@@ -0,0 +1,8 @@
+public class LearnLoops {
+    public static void main(String[] args) {
+        // For loop to print numbers from 0 to 5
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
