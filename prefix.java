2d80bd65841294bdabdaa92371ce9b778d8b699a
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-07-09 20:27:13 +0530

    missing number

--- prefix.java
+++ prefix.java
@@ -1,9 +1,9 @@
 public class prefix {
     public static String longestCommonPrefix(String[] strs) {
-        String prefix = strs [0];
-        for (String s : strs)
-        while(!s.startsWith(prefix))
-        prefix=prefix.substring(0,prefix.length()-1);
+        String prefix = strs [0];// first word as a input 
+        for (String s : strs)// loop with th each word the array
+        while(!s.startsWith(prefix))//loops work prefix start with s
+        prefix=prefix.substring(0,prefix.length()-1);//checking the word from the behind -1
         return(prefix);
     }
     public static void main(String[] args) {
