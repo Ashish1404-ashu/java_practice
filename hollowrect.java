ba9279b557345ad17650420a304978214aa0cfaa
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-07-11 20:27:44 +0530

    A

--- hollowrect.java
+++ hollowrect.java
@@ -1,18 +1,18 @@
 public class hollowrect {
     public static void main(String[] args) {
-        int rows = 5;       // Height of rectangle
-        int columns = 10;   // Width of rectangle
+        int rows = 5;       
+        int columns = 10;   
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= columns; j++) {
-                // Print '*' for first and last row, or first and last column
+            
                 if (i == 1 || i == rows || j == 1 || j == columns) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
-            System.out.println(); // Move to the next line after each row
+            System.out.println(); /
         }
     }
 }
--- star.java
+++ star.java
@@ -1,13 +1,21 @@
 public class star {
     public static void main(String[] args) {
-        int n =10;
+        int row = 5;
+        int column=10;
 
-        for(int i=0; i<=n ;i++){
+
+        for(int i=1; i<=row ;i++){
+            for(int j=1;j<=column;j++){
 
             }
-            System.out.println("*");
+            
         }
-        
+
+  
+    System.out.println("*");
+    }
+    
+    
 }
 
 
