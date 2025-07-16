379f4db17360bb3d1a30f5c64fb22dfadfd39fbe
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-07-16 18:45:01 +0530

    find and check

---
+++ solutions.java
@@ -0,0 +1,16 @@
+public class solutions {
+    public static void main (String []args){
+        int marks []={1,2,3,4,5};
+        int num = 4;
+        for (int element:marks)
+        if (num == element){
+        System.out.println("available");}
+
+        else {
+            System.out.println("not available");
+        }
+        System.out.println();
+
+        }
+        
+    }
