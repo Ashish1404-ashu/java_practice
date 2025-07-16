fe0a00d460ac0982a5c00f977ea5274caad43d9a
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2025-07-16 18:56:50 +0530

    commit

---
+++ solu.java
@@ -0,0 +1,14 @@
+class solu {
+
+    public static void main(String[] args) {
+        float marks []= {1.02f,2.14f,3.25f,4.357f};
+        float sum =0;
+        for (float element:marks){
+
+        
+        sum = sum + element;
+        }
+        System.out.println("the sum of all the number is "+ sum);
+    }
+    
+}
