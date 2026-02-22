cea2b71a868038cfa0acc3949204dff3ad806db2
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2026-02-23 02:25:32 +0530

    age adult or child

---
+++ compareage.java
@@ -0,0 +1,11 @@
+public class compareage {
+public static void main(String[] args) {
+    int n = 17;
+    if (n>18){
+        System.out.println(".(child)");
+    }
+    else{
+        System.out.println(".(adult )");
+    }
+}
+}
--- even.java
+++ even.java
@@ -2,11 +2,11 @@ public class even {
     public static void main(String args[]){
         int n = 3;
         if (n %2 ==0){
-System.out.println(n+"even");
+System.out.println(n+ "even");
         }
 
         else{
-            System.out.println(n+".(odd)");
+            System.out.println(n+"(odd)");
         }
     }
 }
