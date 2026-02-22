fc8c772a646c2ce298a07dfe29239d25c2f63f7f
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2026-02-23 02:44:54 +0530

    income tacx xalculator

---
+++ incometaxcal.java
@@ -0,0 +1,21 @@
+public class incometaxcal {
+    public static void main (String args []){
+        int income = 25000000;
+
+ int tax ;
+        if (income<=500000){
+            tax = 0;
+        }
+
+        else if (income >=500000 && income<=1000000){
+            tax = (int) (income*0.2);
+        }
+
+        else {
+          tax = (int) (income*0.3);
+        }
+        System.out.println(tax+ ".(tax )");
+
+
+        }
+    }
