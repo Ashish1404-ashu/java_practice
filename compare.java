35cb58124714b69d0f7f164ab7b4e0ea38373ee0
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2026-02-23 02:18:41 +0530

    compare two number

---
+++ compare.java
@@ -0,0 +1,17 @@
+public class compare {
+    public static void main(String[] args) {
+        int a = 10;
+        int b=20;
+
+        if (a>b){
+
+        
+        System.out.println(a);
+
+    }
+
+    else{
+        System.out.println("b is "+b);
+    }
+}
+}
