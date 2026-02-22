362cb8c3169821f369e469560195790f9899e775
Ashish1404-ashu <ashishrajverma1404@gmail.com>
2026-02-23 02:23:07 +0530

    even or add

---
+++ even.java
@@ -0,0 +1,12 @@
+public class even {
+    public static void main(String args[]){
+        int n = 3;
+        if (n %2 ==0){
+System.out.println(n+"even");
+        }
+
+        else{
+            System.out.println(n+".(odd)");
+        }
+    }
+}
